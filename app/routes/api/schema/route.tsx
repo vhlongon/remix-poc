251e769eaabf3cddcc2d4f6ad520c4f1d2f6df10
vhlongon <vhlongon@gmail.com>
2023-09-08 17:11:50 +0200

    remove component from api/schema route

--- app/routes/api/schema/route.tsx
+++ app/routes/api/schema/route.tsx
@@ -1,28 +1,7 @@
 import type { LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
 import schema from './schema.json';
 
 export const loader = async ({ request }: LoaderArgs) => {
-  const { info, servers } = schema;
-  return json({ info, servers }, 200);
+  return json(schema, 200);
 };
-
-export default function SchemaPage() {
-  const data = useLoaderData<typeof loader>();
-
-  return (
-    <>
-      <h1>{data.info.title}</h1>
-      <ul>
-        {data.servers.map(({ url, description }) => (
-          <li key={url}>
-            <a href={url} target="_blank" rel="noreferrer">
-              {description}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-}
